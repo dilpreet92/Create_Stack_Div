74760cd946503de058c829b76e05b7383f942437
Dilpreet Singh <dilpreet@vinsol.com>
2014-07-28 19:47:08 +0530

    Adding the changes as dicussed

--- js/create_stack.js
+++ js/create_stack.js
@@ -2,34 +2,38 @@ function CreateStack() {
   this.count = 0 ;
   this.bindEvents();
 }
+
 CreateStack.prototype.addDivTag = function() {
   this.count += 1;
-  this.currentDiv = $("<div/>").addClass("innerClass")
+  var currentDiv = $("<div/>").addClass("innerClass")
                                .attr("id", "divInner"+ this.count)
                                .appendTo(".container");
 };
 
-CreateStack.prototype.removeDivTag = function() {
-  this.count -= 1;
-  var currentDivElement = this.currentDiv;
-  this.currentDiv = this.currentDiv.prev();
-  currentDivElement.remove();
-};
-
-CreateStack.prototype.bindEvents = function() {
+CreateStack.prototype.addItemHandler = function() {
   var _this = this;
   $("#addStack").on("click", function(){
     _this.addDivTag();
   });
+};
+
+CreateStack.prototype.highlightAndRemoveHandler = function() {
+  var _this = this;  
   $(".container").on("click",".innerClass" ,function() {
     $(".highlight").removeClass("highlight");
-    if(this.id == _this.currentDiv.attr("id")) {
-      _this.removeDivTag();
+    if(this.id == $(".innerClass:last").attr("id")) {
+      this.remove();
+      _this.count -= 1;
     }
     else {
       $(this).addClass("highlight");
     } 
   });
+};
+
+CreateStack.prototype.bindEvents = function() {
+  this.addItemHandler();
+  this.highlightAndRemoveHandler();
 };
 
 $(document).ready(function() {
